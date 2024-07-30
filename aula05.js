f5ac6ad68e429fff21ab63b7a195208c8bede701
Nikolas Arruda Caracho <nikolasarruda762@gmail.com>
2024-07-30 20:00:55 -0300

    Update aula05.js

--- aula05.js
+++ aula05.js
@@ -12,7 +12,21 @@ cores.splice(3, 2); // a partir do elemento 3, remove 2 elementos.
 console.log(cores);
 
 
-for (let i = 0; i <= 10000; i= 1 + i*2 ) //inicializador; verificador; incrementador.
+for (let i = 0; i < cores.lenght; i++ ) //inicializador; verificador; incrementador.
 {
-    console.log(i)
+    let cor = cores[i];
+    console.log(cor);
+
+}
+
+for (let cor of cores) {// itera o vetor
+    console.log(cor);
+}
+
+
+let i = 0;
+while (i < cores.length){
+    let cor = cores [i];
+    console.log(cor);
+    i++;
 }
