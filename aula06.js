8bf02f73b5dc58fb4876fd3e81f58bb56cbf8015
Nikolas Arruda Caracho <nikolasarruda762@gmail.com>
2024-08-01 21:11:10 -0300

    ,

---
+++ aula06.js
@@ -0,0 +1,24 @@
+const fila = [];
+
+//adicionar elementos na fila
+fila.push(1);
+fila.push(2);
+fila.push(3);
+
+// Remover elementos da fila
+
+console.log (fila.shift());
+console.log (fila.shift());
+console.log (fila.shift());
+
+const pilha = [];
+
+//Adicionar elementos na pilha
+
+pilha.push(1);
+pilha.push(2);
+pilha.push(3);
+
+pilha.pop();
+pilha.pop();
+pilha.pop();
---
+++ exemplosaula06/exemplo01.js
@@ -0,0 +1,5 @@
+/* Crie um conjunto de funções para simular o comportamento de um histórico de navegação de um navegador.
+A função navegarPara deve adicionar uma nova página ao histórico. A função voltar deve remover a última
+página visitada do histórico e retornar a página anterior. E a função paginaAtual deve retornar a página
+atual.
+ */
